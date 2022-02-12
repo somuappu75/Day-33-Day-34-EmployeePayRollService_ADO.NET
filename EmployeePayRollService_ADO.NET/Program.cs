3cb8b1c69c2a81982b8e2fa9bb752aff4dd8091c
Somanath Havinal <somusp75@gmail.com>
2022-02-12 18:59:43 +0530

    Add project files.

---
+++ EmployeePayRollService_ADO.NET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayRollService_ADO.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
